93817c42a5711819cbc1f0fc7aeae232c965862c
FlutLab.io <>
2022-07-15 17:13:21 +0000

    Autosave

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -82,6 +82,74 @@ class HomeScreen extends StatelessWidget {
                 ),
               ],
             ),
+            AppBar(
+              backgroundColor: Colors.blue[900],
+              elevation: 5.0,
+              leading: InkResponse(
+                onTap: () {},
+                child: Icon(
+                  Icons.menu,
+                  size: 30,
+                  color: Colors.black,
+                ),
+              ),
+              title: Padding(
+                padding: const EdgeInsets.only(right: 18.0, top: 10),
+                child: Image(
+                  image: AssetImage("assets/images/amazon_logo.jpg"),
+                  height: 30,
+                ),
+              ),
+              centerTitle: true,
+              actions: <Widget>[
+                Stack(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 10, right: 20),
+                      child: InkResponse(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (_) => CartScreen()));
+                        },
+                        child: Icon(
+                          Icons.shopping_basket,
+                          size: 30,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 8,
+                      right: 16,
+                      child: Container(
+                        height: 20,
+                        width: 20,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.orange,
+                        ),
+                        child: Center(
+                          child: Text(
+                            '5',
+                            style: TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 20),
+                  child: InkResponse(
+                    onTap: () {},
+                    child: Icon(
+                      Icons.search,
+                      size: 30,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
