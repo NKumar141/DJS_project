d288d2a03adec4cf72220daa4fd9d031806bd9e8
FlutLab.io <>
2022-07-16 02:31:10 +0000

    Autosave

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(80.0),
+        preferredSize: const Size.fromHeight(90.0),
         child: Container(
             decoration: const BoxDecoration(
               gradient: LinearGradient(
