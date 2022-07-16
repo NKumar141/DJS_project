e40a6b38bcb779a12ac180a5fabdfc183949c348
FlutLab.io <>
2022-07-16 08:08:53 +0000

    Autosave

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -79,6 +79,22 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+      ),
     );
   }
 }
