77e726aeea1bbf5e8eb8373e3b392d92f284b3c2
FlutLab.io <>
2022-07-16 06:02:00 +0000

    Autosave

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -14,25 +14,25 @@ class Product {
 
 final List<Product> products = [
   Product(
-    imageUrl: 'assets/images/photo.jpg',
+    imageUrl: 'assets/images/photo.png',
     name: 'Alabama Theatre',
     price: 79.95,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Auctor neque vitae tempus quam pellentesque nec. Volutpat consequat mauris nunc congue nisi. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Aliquam id diam maecenas ultricies mi eget.',
   ),
   Product(
-    imageUrl: 'assets/images/photojur.jpg',
+    imageUrl: 'assets/images/photojur.png',
     name: 'Jurasic Miniature Golf',
     price: 109.99,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Auctor neque vitae tempus quam pellentesque nec. Volutpat consequat mauris nunc congue nisi. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Aliquam id diam maecenas ultricies mi eget.',
   ),
   Product(
-    imageUrl: 'assets/images/photopiz.jpg',
+    imageUrl: 'assets/images/photopiz.png',
     name: 'Pizza Place',
     price: 1199.99,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Auctor neque vitae tempus quam pellentesque nec. Volutpat consequat mauris nunc congue nisi. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Aliquam id diam maecenas ultricies mi eget.',
   ),
   Product(
-    imageUrl: 'assets/images/photomyr.jpg',
+    imageUrl: 'assets/images/photomyr.png',
     name: 'Myrtle Beach Skywheel',
     price: 10.20,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Auctor neque vitae tempus quam pellentesque nec. Volutpat consequat mauris nunc congue nisi. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Aliquam id diam maecenas ultricies mi eget.',
@@ -41,19 +41,19 @@ final List<Product> products = [
 
 final List<Product> books = [
   Product(
-    imageUrl: 'assets/images/photomr.jpg',
+    imageUrl: 'assets/images/photomr.png',
     name: 'Mr. Fish Seafood Grill',
     price: 18.49,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Auctor neque vitae tempus quam pellentesque nec. Volutpat consequat mauris nunc congue nisi. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Aliquam id diam maecenas ultricies mi eget.',
   ),
   Product(
-    imageUrl: 'assets/images/photojoe.jpg',
+    imageUrl: 'assets/images/photojoe.png',
     name: "Joe's Crab Shack",
     price: 18.40,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Auctor neque vitae tempus quam pellentesque nec. Volutpat consequat mauris nunc congue nisi. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Aliquam id diam maecenas ultricies mi eget.',
   ),
   Product(
-    imageUrl: 'assets/images/photodir.jpg',
+    imageUrl: 'assets/images/photodir.png',
     name: 'Dirty Dons Oyester Bar and Gril',
     price: 18.98,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Auctor neque vitae tempus quam pellentesque nec. Volutpat consequat mauris nunc congue nisi. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Aliquam id diam maecenas ultricies mi eget.',
