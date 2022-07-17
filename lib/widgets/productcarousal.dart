9850146134788329327c3749973b8ab8b0d41df6
FlutLab.io <>
2022-07-17 12:39:37 +0000

    Autosave

--- lib/widgets/productcarousal.dart
+++ lib/widgets/productcarousal.dart
@@ -11,7 +11,7 @@ class ProductCarousal extends StatelessWidget {
       //margin: EdgeInsets.all(4),
       padding: EdgeInsets.all(4),
       width: 250,
-      height: 300,
+      height: 400,
       // decoration: BoxDecoration(
       //   color: Colors.white,
       //   boxShadow: [
