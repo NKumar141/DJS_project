09b45322ce061e0ac645a6cada72b6c8a04da011
FlutLab.io <>
2022-07-17 20:19:34 +0000

    Autosave

--- lib/models/detail.dart
+++ lib/models/detail.dart
@@ -24,7 +24,7 @@ class _MyHomePageState extends State<yes> {
               SizedBox(
                 width: 20,
               ),
-              Text('The Alabama Theatre', style: TextStyle(color: Colors.grey[800], fontWeight: FontWeight.bold, fontSize: 40)),
+              Text('The Alabama Theatre', style: TextStyle(color: Colors.grey[800], fontWeight: FontWeight.bold, fontSize: 20)),
               Image.network(
                 'https://png.pngtree.com/png-clipart/20220429/original/pngtree-glossy-heart-best-vector-ai-and-png-png-image_7581956.png',
                 height: 30,
