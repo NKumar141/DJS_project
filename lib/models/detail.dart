b80c265806a6e9158fcff741922377677279cb8b
FlutLab.io <>
2022-07-17 20:08:47 +0000

    Autosave

--- lib/models/detail.dart
+++ lib/models/detail.dart
@@ -2,8 +2,12 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import '../screens/location.dart';
 
-class detail extends StatelessWidget {
-  // This widget is the root of your application.
+class yes extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<yes> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +19,23 @@ class detail extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class detail extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryIconTheme: Theme.of(context).primaryIconTheme.copyWith(
+              color: Colors.black,
+            ),
+      ),
+      home: yes(),
     );
   }
 }
