3cdbcc6a9031844965422fddfcd9d2af2a9d50cf
FlutLab.io <>
2022-07-17 16:44:09 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -84,6 +84,9 @@ class signin extends StatelessWidget {
                   height: 20,
                 ),
                 OutlinedButton(
+                  borderSide: BorderSide(
+          height 70,
+        ),
                   onPressed: () {
                     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => location()));
                   },
